45ebaf7c1eb679ed6c4d015c2819a8a6ddbcab6b
Chanhee Jeong <chan420420@gmail.com>
2022-12-20 02:19:48 +0900

    ✨ Add TabBar

---
+++ HRHN/View/VC/RecordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordViewController.swift
+//  HRHN
+//
+//  Created by Chanhee Jeong on 2022/12/20.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = .systemCyan
+        super.viewDidLoad()
+    }
+
+}
---
+++ HRHN/View/VC/TabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarController.swift
+//  HRHN
+//
+//  Created by Chanhee Jeong on 2022/12/20.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setUpTabBar()
+    }
+
+    private func setUI() {
+        tabBar.tintColor = UIColor.red
+        tabBar.unselectedItemTintColor = UIColor.gray
+        tabBar.backgroundColor = UIColor.white
+    }
+    
+    private func setUpTabBar() {
+        let todayVC = TodayViewController()
+        let firstTab = UINavigationController(rootViewController: todayVC)
+        firstTab.tabBarItem = UITabBarItem(title: "오늘의 챌린지", image: UIImage(systemName: "sparkles"), selectedImage: UIImage(systemName: "sparkles"))
+        
+        let recordVC = RecordViewController()
+        let secondTab = UINavigationController(rootViewController: recordVC)
+        secondTab.tabBarItem = UITabBarItem(title: "지난 챌린지", image: UIImage(systemName: "list.bullet.circle"), selectedImage: UIImage(systemName: "list.bullet.circle.fill"))
+        
+        viewControllers = [firstTab, secondTab]
+        
+    }
+
+}
