2a61608333897ea109c7c46025bbdc491d17399f
Chanhee Jeong <chan420420@gmail.com>
2022-12-23 23:11:44 +0900

    ♻️ Make Challenge Hashable

--- HRHN/Model/Challenge.swift
+++ HRHN/Model/Challenge.swift
@@ -17,9 +17,44 @@ enum Emoji: String, Codable {
     case none = "none"
 }
 
-struct Challenge {
+extension Emoji: Hashable, Equatable {
+    public var hashValue: Int {
+        switch self {
+        case .red: return 0
+        case .yellow: return 1
+        case .green: return 2
+        case .skyblue: return 3
+        case .blue: return 4
+        case .purple: return 5
+        case .none: return 6
+        }
+    }
+    
+    public static func ==(lhs: Emoji, rhs: Emoji) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+}
+
+
+struct Challenge: Hashable, Equatable {
     let id: UUID
     let date: Date
     let content: String
     let emoji: Emoji
+    
+    init(id: UUID, date: Date, content: String, emoji: Emoji) {
+        self.id = id
+        self.date = date
+        self.content = content
+        self.emoji = emoji
+    }
+    
+    func hash(into hasher: inout Hasher) {
+      hasher.combine(id)
+    }
+
+    static func == (lhs: Challenge, rhs: Challenge) -> Bool {
+      lhs.id == rhs.id
+    }
+    
 }
--- HRHN/Model/MockData.swift
+++ HRHN/Model/MockData.swift
@@ -9,20 +9,13 @@ import Foundation
 
 extension Challenge {
     static let mock: [Challenge] = [
-        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
-        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
-        Challenge(id: UUID().uuidString, date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue),
-        Challenge(id: UUID().uuidString, date: Date(), content: "The way to get started is to quit talking and begin doing. -Walt Disney", emoji: Emoji.red),
-        Challenge(id: UUID().uuidString, date: Date(), content: "Life is what happens when you're busy making other plans. -John Lennon", emoji: Emoji.yellow),
-        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
-        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
-        Challenge(id: UUID().uuidString, date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue),
-        Challenge(id: UUID().uuidString, date: Date(), content: "The way to get started is to quit talking and begin doing. -Walt Disney", emoji: Emoji.red),
-        Challenge(id: UUID().uuidString, date: Date(), content: "Life is what happens when you're busy making other plans. -John Lennon", emoji: Emoji.yellow),
-        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
-        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
-        Challenge(id: UUID().uuidString, date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue),
-        Challenge(id: UUID().uuidString, date: Date(), content: "The way to get started is to quit talking and begin doing. -Walt Disney", emoji: Emoji.red),
-        Challenge(id: UUID().uuidString, date: Date(), content: "Life is what happens when you're busy making other plans. -John Lennon", emoji: Emoji.yellow)
+        Challenge(id: UUID(), date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
+        Challenge(id: UUID(), date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
+        Challenge(id: UUID(), date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue),
+        Challenge(id: UUID(), date: Date(), content: "The way to get started is to quit talking and begin doing. -Walt Disney", emoji: Emoji.red),
+        Challenge(id: UUID(), date: Date(), content: "Life is what happens when you're busy making other plans. -John Lennon", emoji: Emoji.yellow),
+        Challenge(id: UUID(), date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
+        Challenge(id: UUID(), date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
+        Challenge(id: UUID(), date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue)
     ]
 }
