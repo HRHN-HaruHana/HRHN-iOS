7c35500ca1368d65ff62a030c2e940c06545cec4
Chanhee Jeong <chan420420@gmail.com>
2022-12-23 21:20:46 +0900

    🔥 Delete code to test coredata

--- HRHN/View/VC/TodayViewController.swift
+++ HRHN/View/VC/TodayViewController.swift
@@ -31,44 +31,7 @@ final class TodayViewController: UIViewController {
 extension TodayViewController {
     
     @objc func testDidTap(_ sender: UIButton) {
-        /*
-        let coreDataManger = CoreDataManager.shared
-        coreDataManger.insertChallenge(Challenge(id: UUID(),
-                                          date: tomorrow,
-                                          content: "안녕하세요",
-                                          emoji: Emoji.blue))
-
-        // 코어데이터 싱글톤 매니저
-        let coreDataManger = CoreDataManager.shared
-        
-        // 챌린지 등록
-        coreDataManger.insertChallenge(Challenge(id: UUID(),
-                                          date: Date(),
-                                          content: "안녕하세요",
-                                          emoji: Emoji.blue))
-        
-        // 챌린지 전체 가져오기 (최신순)
-        coreDataManger.getChallenges()
-        
-        // 특정날짜 챌린지만 가져오기
-        // let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
-        coreDataManger.getChallengeOf(Date())
-        
-        // 가장최근 챌린지 가져오기
-        coreDataManger.getChallenges()[0]
-        
-        // 챌린지 업데이트
-        coreDataManger.updateChallenge(Challenge(id: UUID(),
-                                          date: Date(),
-                                          content: "안녕히계세요",
-                                          emoji: Emoji.green))
-        
-        // 특정날짜의 챌린지 삭제
-        coreDataManger.deleteChallenge(Date())
-        
-        // 전체 챌린지 삭제
-        coreDataManger.deleteAllChallenges()
-        */
+        // TODO: - Delete
     }
     
 }
