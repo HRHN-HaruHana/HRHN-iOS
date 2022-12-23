3a2eb829388283d82f26f444f077616d1ccb75fd
Chanhee Jeong <chan420420@gmail.com>
2022-12-23 22:22:17 +0900

    🤡 Add mockdata

---
+++ HRHN/Model/MockData.swift
@@ -0,0 +1,28 @@
+//
+//  MockData.swift
+//  HRHN
+//
+//  Created by Chanhee Jeong on 2022/12/23.
+//
+
+import Foundation
+
+extension Challenge {
+    static let mock: [Challenge] = [
+        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
+        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
+        Challenge(id: UUID().uuidString, date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue),
+        Challenge(id: UUID().uuidString, date: Date(), content: "The way to get started is to quit talking and begin doing. -Walt Disney", emoji: Emoji.red),
+        Challenge(id: UUID().uuidString, date: Date(), content: "Life is what happens when you're busy making other plans. -John Lennon", emoji: Emoji.yellow),
+        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
+        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
+        Challenge(id: UUID().uuidString, date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue),
+        Challenge(id: UUID().uuidString, date: Date(), content: "The way to get started is to quit talking and begin doing. -Walt Disney", emoji: Emoji.red),
+        Challenge(id: UUID().uuidString, date: Date(), content: "Life is what happens when you're busy making other plans. -John Lennon", emoji: Emoji.yellow),
+        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.none),
+        Challenge(id: UUID().uuidString, date: Date(), content: "삶이 있는 한 희망은 있다 -키케로", emoji: Emoji.green),
+        Challenge(id: UUID().uuidString, date: Date(), content: "산다는것 그것은 치열한 전투이다. -로망로랑", emoji: Emoji.blue),
+        Challenge(id: UUID().uuidString, date: Date(), content: "The way to get started is to quit talking and begin doing. -Walt Disney", emoji: Emoji.red),
+        Challenge(id: UUID().uuidString, date: Date(), content: "Life is what happens when you're busy making other plans. -John Lennon", emoji: Emoji.yellow)
+    ]
+}
