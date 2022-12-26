29007d3f5d0b929a5a69e70e270a5872aad22019
Chaeho-Min <james118@naver.com>
2022-12-26 18:00:44 +0900

    ✨ Add ReviewView

---
+++ HRHN/View/UI/SwiftUIView/ReviewView.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewView.swift
+//  HRHN
+//
+//  Created by 민채호 on 2022/12/26.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewView()
+    }
+}
