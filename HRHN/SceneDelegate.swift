9b577d56b3f767107a013d88306e2a6a8a227f24
Chanhee Jeong <chan420420@gmail.com>
2022-12-28 17:18:57 +0900

    ✨ Add Setting TableView

--- HRHN/SceneDelegate.swift
+++ HRHN/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             guard let windowScene = (scene as? UIWindowScene) else { return }
             self.window = UIWindow(windowScene: windowScene)
             
-            let initialViewController = TabBarController()
+            let initialViewController = SettingViewController()
             self.window?.rootViewController = initialViewController
             self.window?.makeKeyAndVisible()
         }
