b18c0bc6a044d377d29188b120868bdc4f8e6416
Chaeho-Min <james118@naver.com>
2022-12-29 18:33:27 +0900

    ✨ Change CoreDataManager targets & Fix code

---
+++ HRHN/Extension/URL+.swift
@@ -0,0 +1,18 @@
+//
+//  URL+.swift
+//  HRHN
+//
+//  Created by 민채호 on 2022/12/29.
+//
+
+import Foundation
+
+extension URL {
+    static func storeURL(for appGroup: String, databaseName: String) -> URL {
+        guard let fileContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup) else {
+            fatalError("Shared file container could not be created.")
+        }
+
+        return fileContainer.appendingPathComponent("\(databaseName).sqlite")
+    }
+}
