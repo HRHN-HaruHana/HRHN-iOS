b8c911ad57f9008255e234d655ec6f1aca3a5dae
Chaeho-Min <james118@naver.com>
2022-12-29 21:40:41 +0900

    ✨ Add Widget Timeline logic

--- LockscreenWidget/LockscreenWidget.swift
+++ LockscreenWidget/LockscreenWidget.swift
@@ -22,15 +22,21 @@ struct Provider: IntentTimelineProvider {
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let midnight = Calendar.current.startOfDay(for: currentDate)
+        let nextMidnight = Calendar.current.date(
+            byAdding: .day,
+            value: 1,
+            to: midnight
+        )!
+        
+        let entry = SimpleEntry(
+            date: currentDate,
+            configuration: configuration
+        )
+        entries.append(entry)
+        
+        let timeline = Timeline(entries: entries, policy: .after(nextMidnight))
         completion(timeline)
     }
 }
