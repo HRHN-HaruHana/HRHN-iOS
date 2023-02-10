469250fba09f6d7e13787bba3eabcfc2923a924c
Chaeho-Min <james118@naver.com>
2023-02-10 20:36:17 +0900

    ✨ Add TabIcons enum

---
+++ HRHN/Sources/Utils/Design/TabIcons/TabIcons.swift
@@ -0,0 +1,17 @@
+//
+//  TabIcons.swift
+//  HRHN
+//
+//  Created by 민채호 on 2023/02/10.
+//
+
+import Foundation
+
+enum TabbarIcons: String {
+    case todaySelected = "Today_selected"
+    case todayUnselected = "Today_unselected"
+    case recordSelected = "Record_selected"
+    case recordUnselected = "Record_unselected"
+    case storageSelected = "Storage_selected"
+    case storageUnselected = "Storage_unselected"
+}
