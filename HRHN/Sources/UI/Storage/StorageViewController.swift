114c7f391ba1305b5614e02ec54203e577024178
Chanhee Jeong <chan420420@gmail.com>
2023-02-20 03:22:21 +0900

    ✨ Add TableView and Header

--- HRHN/Sources/UI/Storage/StorageViewController.swift
+++ HRHN/Sources/UI/Storage/StorageViewController.swift
@@ -13,6 +13,16 @@ final class StorageViewController: UIViewController {
     // MARK: - Properties
     private let viewModel: StorageViewModel
 
+    private lazy var tableView: UITableView = { [weak self] in
+        $0.backgroundColor = .clear
+        $0.separatorStyle = .none
+        $0.showsVerticalScrollIndicator = false
+        $0.register(ChallengeCell.self, forCellReuseIdentifier: "ChallengeCell")
+        $0.register(StorageHeaderView.self, forHeaderFooterViewReuseIdentifier: "StorageHeaderView")
+        $0.delegate = self
+        $0.dataSource = self
+        return $0
+    }(UITableView(frame: .zero, style: .grouped))
     
     // MARK: - LifeCycle
     
@@ -33,6 +43,8 @@ final class StorageViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        viewModel.fetchPreviousChallenges()
+        tableView.reloadData()
     }
     
 }
@@ -46,18 +58,15 @@ extension StorageViewController {
     }
 }
 
-// MARK: - Bindings
-extension StorageViewController {
-    
-    private func bind() {
-
-    }
-}
 
 // MARK: - UI Functions
 extension StorageViewController {
     private func setUI(){
         view.backgroundColor = .background
+        view.addSubviews(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
     
     private func setNavigationBar() {
@@ -65,3 +74,63 @@ extension StorageViewController {
         navigationItem.rightBarButtonItem = rightBarButton
     }
 }
+
+// MARK: - UITableViewDataSource
+
+extension StorageViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.challenges.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ChallengeCell", for: indexPath)
+                as? ChallengeCell else { return UITableViewCell() }
+        cell.configure(with: viewModel.challenges.value[indexPath.row])
+        return cell
+        
+    }
+
+}
+
+// MARK: - UITableViewDelegate
+extension StorageViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 109 + 20
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "StorageHeaderView") as? StorageHeaderView else {
+            return UIView()
+        }
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let reviewVC = ReviewViewController(viewModel: ReviewViewModel(
+            from: .recordTab,
+            challenge: viewModel.challenges.value[indexPath.row],
+            navigationController: self.navigationController
+        ))
+        reviewVC.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(reviewVC, animated: true)
+    }
+
+}
+
+
+#if DEBUG
+import SwiftUI
+
+struct StorageViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        StorageViewController(viewModel: StorageViewModel()).toPreview()
+    }
+}
+#endif
--- HRHN/Sources/UI/Storage/StorageViewModel.swift
+++ HRHN/Sources/UI/Storage/StorageViewModel.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 final class StorageViewModel {
+    var challenges: Observable<[Challenge]> = Observable([])
+    
+    private var coreDataManager = CoreDataManager.shared
+    
     init(){}
+    
+    func fetchPreviousChallenges() {
+        let challenges = self.coreDataManager.getChallenges().filter { (challenge: Challenge) -> Bool in
+            let current = Calendar.current
+            return !current.isDateInToday(challenge.date)
+        }
+        self.challenges = Observable(challenges)
+    }
 }
 
